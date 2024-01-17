1225406a149c12a8c094c037bc18b9ea44788891
meganying1 <74328643+meganying1@users.noreply.github.com>
2024-01-16 21:13:50 -0800

    Update 680: Valid Palindrome II.py

--- Two Pointers/680: Valid Palindrome II.py
+++ Two Pointers/680: Valid Palindrome II.py
@@ -8,6 +8,26 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
         start, end = 0, len(s)-1
+
+        def isPalindrome(s, start, end):
+            while start < end:
+                if s[start] != s[end]: return False
+                start += 1
+                end -= 1
+            return True
+
+        while start < end:
+            if s[start] == s[end]:
+                start += 1
+                end -= 1
+                continue
+            return isPalindrome(s, start+1, end) or isPalindrome(s, start, end-1)
+        return True
+
+"""
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        start, end = 0, len(s)-1
         deleted = False
         while start < end:
             if s[start] == s[end]:
@@ -17,3 +37,5 @@ class Solution:
                 str1, str2 = s[start:end], s[start+1:end+1]
                 return str1 == str1[::-1] or str2 == str2[::-1]
         return True
+"""
+# check for palindrome using two pointers to ensure entire solution is O(1) space
