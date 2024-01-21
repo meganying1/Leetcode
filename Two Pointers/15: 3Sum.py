ee4b8b06b3fed74bccbaa41b6dadab4c8fcaec90
meganying1 <74328643+meganying1@users.noreply.github.com>
2024-01-21 11:18:57 -0800

    Update 15: 3Sum.py

--- Two Pointers/15: 3Sum.py
+++ Two Pointers/15: 3Sum.py
@@ -8,6 +8,29 @@
 
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        ans = []
+        length = len(nums)
+        nums.sort()
+        for i in range(length):
+            if i != 0 and nums[i] == nums[i-1]: continue
+            neededSum = -nums[i]
+            j, k = i+1, length-1
+            while j < k:
+                currSum = nums[j] + nums[k]
+                if currSum == neededSum:
+                    ans.append((nums[i], nums[j], nums[k]))
+                    j += 1 # you know you will need to increment j regardless
+                    while j < length and nums[j] == nums[j-1]: j += 1 # increment j until it's unique
+                elif currSum > neededSum: k -= 1
+                else: j += 1
+        return ans
+# time complexity: O(n^2)
+# space complexity:L O(sort)
+#     sort() in python is O(n) space complexity, but don't assume and say that space complexity depends on the sort function
+
+"""
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
         ans = set()
         length = len(nums)
         nums.sort()
@@ -24,6 +47,9 @@ class Solution:
                 elif currSum > neededSum: k -= 1
                 else: j += 1
         return list(ans)
+"""
+# solution uses O(n^2) space for ans in addition to return O(n^2) space, since we are not returning ans
+#    -> never use set() to get rid of duplicates
 
 """
 class Solution:
