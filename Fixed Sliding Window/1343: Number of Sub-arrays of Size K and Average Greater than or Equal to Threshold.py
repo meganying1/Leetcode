cc5730d74a67e3fd3a5d1b7828aba099c01fe761
meganying1 <74328643+meganying1@users.noreply.github.com>
2024-01-25 18:55:12 -0800

    Update 1343: Number of Sub-arrays of Size K and Average Greater than or Equal to Threshold.py

--- Fixed Sliding Window/1343: Number of Sub-arrays of Size K and Average Greater than or Equal to Threshold.py
+++ Fixed Sliding Window/1343: Number of Sub-arrays of Size K and Average Greater than or Equal to Threshold.py
@@ -10,6 +10,23 @@ class Solution:
         minSum = k * threshold
         length = len(arr)
         start, end = 0, k-1
+        currSum = sum(arr[i] for i in range(k))
+        ans = 0
+        while end < length:
+            if currSum >= minSum: ans += 1
+            if end+1 == length: break
+            currSum -= arr[start]
+            currSum += arr[end+1]
+            start += 1
+            end += 1
+        return ans
+        
+"""
+class Solution:
+    def numOfSubarrays(self, arr: List[int], k: int, threshold: int) -> int:
+        minSum = k * threshold
+        length = len(arr)
+        start, end = 0, k-1
         currSum = sum(arr[start:end+1])
         ans = 1 if currSum >= minSum else 0
         while end < length-1:
@@ -19,3 +36,5 @@ class Solution:
             end += 1
             if currSum >= minSum: ans += 1
         return ans
+"""     
+# when writing currSum = sum(arr[start:end+1]), an array of size end-start is created in memory temporarily, increasing space complexity
