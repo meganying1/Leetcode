e881aac26c89799adc4bc618e5bf81d4aadc167c
meganying1 <74328643+meganying1@users.noreply.github.com>
2024-01-25 18:45:22 -0800

    Create 1343: Number of Sub-arrays of Size K and Average Greater than or Equal to Threshold.py

---
+++ Fixed Sliding Window/1343: Number of Sub-arrays of Size K and Average Greater than or Equal to Threshold.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/description/
+# difficulty: medium
+# topics: array, sliding window
+
+# problem:
+# Given an array of integers arr and two integers k and threshold, return the number of sub-arrays of size k and average greater than or equal to threshold.
+
+class Solution:
+    def numOfSubarrays(self, arr: List[int], k: int, threshold: int) -> int:
+        minSum = k * threshold
+        length = len(arr)
+        start, end = 0, k-1
+        currSum = sum(arr[start:end+1])
+        ans = 1 if currSum >= minSum else 0
+        while end < length-1:
+            currSum -= arr[start]
+            currSum += arr[end+1]
+            start += 1
+            end += 1
+            if currSum >= minSum: ans += 1
+        return ans
