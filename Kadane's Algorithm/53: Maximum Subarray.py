9bab6b5d0d27b3472b50c1e12bf71a36e9dea362
meganying1 <74328643+meganying1@users.noreply.github.com>
2024-02-11 21:25:35 -0800

    Update 53: Maximum Subarray.py

--- Kadane's Algorithm/53: Maximum Subarray.py
+++ Kadane's Algorithm/53: Maximum Subarray.py
@@ -6,8 +6,23 @@
 
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
+        start = end = 0
+        ans, windowSum = -float("inf"), 0
+        while end < len(nums):
+            windowSum += nums[end]
+            ans = max(ans, windowSum)
+            if windowSum < 0:
+                start = end + 1
+                windowSum = 0
+            end += 1
+        return ans
+
+"""
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
         currSum, maxSum = nums[0], nums[0]
         for i in range(1, len(nums)):
             currSum = max(nums[i], currSum + nums[i])
             maxSum = max(maxSum, currSum)
         return maxSum
+"""
