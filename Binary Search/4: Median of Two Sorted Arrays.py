bf4782603c69f720af6e9fd5a7b5343a6a4aee1b
meganying1 <74328643+meganying1@users.noreply.github.com>
2024-02-27 22:15:11 -0800

    Update 4: Median of Two Sorted Arrays.py

--- Binary Search/4: Median of Two Sorted Arrays.py
+++ Binary Search/4: Median of Two Sorted Arrays.py
@@ -6,7 +6,29 @@
 # Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
 # The overall run time complexity should be O(log (m+n)).
 
-# naive solution:
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        len1, len2 = len(nums1), len(nums2)
+        if len1 > len2:
+            nums1, nums2 = nums2, nums1
+            len1, len2 = len2, len1
+        if len1 == 0:
+            if len2 % 2 == 1: return nums2[len2//2]
+            else: return (nums2[len2//2] + nums2[len2//2 - 1]) / 2
+        lo, hi = 0, len1
+        while lo <= hi:
+            mid1 = (hi+lo+1) // 2
+            mid2 = ((len1 + len2) // 2) - mid1
+            maxLeft1 = nums1[mid1-1] if mid1 != 0 else -float("inf")
+            minRight1 = nums1[mid1] if mid1 != len1 else float("inf")
+            maxLeft2 = nums2[mid2-1] if mid2 != 0 else -float("inf")
+            minRight2 = nums2[mid2] if mid2 != len2 else float("inf")
+            if maxLeft1 > minRight2: hi = mid1-1
+            elif minRight1 < maxLeft2: lo = mid1+1
+            else: break
+        if (len1 + len2) % 2 == 1: return min(minRight1, minRight2)
+        return (max(maxLeft1, maxLeft2) + min(minRight1, minRight2)) / 2
+        
 """
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
@@ -29,6 +51,7 @@ class Solution:
         if (length1+length2) % 2 == 1: return merged[(length1+length2)//2]
         else: return (merged[(length1+length2)//2] + merged[((length1+length2)//2)-1])/2
 """
+# this is the naive solution
 # time complexity: O(n+m)
 #     takes O(n+m) time to iterate through both lists and create merged list
 #     takes O(1) time to find middle of merged list
