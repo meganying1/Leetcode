17c4ac39fda364b1ed8331beb42505d62f72e5e9
meganying1 <74328643+meganying1@users.noreply.github.com>
2024-03-09 23:29:17 -0800

    Update 198: House Robber.py

--- Dynamic Programming/198: House Robber.py
+++ Dynamic Programming/198: House Robber.py
@@ -10,8 +10,28 @@ class Solution:
     def rob(self, nums: List[int]) -> int:
         length = len(nums)
         if length <= 2: return max(nums)
+        prev3, prev2, prev1 = 0, nums[0], nums[1]
+        for i in range(3, length+1):
+            temp = prev1
+            prev1 = nums[i-1] + max(prev2, prev3)
+            prev3 = prev2
+            prev2 = temp
+        return max(prev1, prev2)
+# space optimized bottom-up
+# time complexity: O(n)
+# space complexity: O(1)
+
+"""
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        length = len(nums)
+        if length <= 2: return max(nums)
         dp = [0] * (length+1)
         dp[1], dp[2] = nums[0], nums[1]
         for i in range(3, length+1):
             dp[i] = nums[i-1] + max(dp[i-2], dp[i-3])
         return max(dp[-1], dp[-2])
+"""
+# bottom-up
+# time complexity: O(n)
+# space complexity: O(n)
