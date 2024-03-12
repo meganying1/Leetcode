c73fcc83ae9c42664e8ddaa1fab1d01a71d94577
meganying1 <74328643+meganying1@users.noreply.github.com>
2024-03-11 21:04:23 -0700

    Update 5: Longest Palindromic Substring.py

--- Dynamic Programming/5: Longest Palindromic Substring.py
+++ Dynamic Programming/5: Longest Palindromic Substring.py
@@ -8,6 +8,27 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         length = len(s)
+        ansStart = 0
+        ansEnd = 0
+        
+        def checkPal(start, end):
+            nonlocal ansStart
+            nonlocal ansEnd
+            while start >= 0 and end < length and s[start] == s[end]:
+                if (end-start+1) > (ansEnd-ansStart+1):
+                    ansStart, ansEnd = start, end
+                start -= 1
+                end += 1
+    
+        for i in range(length):
+            checkPal(i, i)
+            checkPal(i, i+1)
+        return s[ansStart:ansEnd+1]
+
+"""
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        length = len(s)
         self.maxLen = 0
         self.ans = ""
         
@@ -23,6 +44,13 @@ class Solution:
             checkPal(i, i)
             checkPal(i, i+1)
         return self.ans
+"""
+# bottom-up
+# time complexity: O(n^3)
+#     for loop takes O(n) time, while loop takes O(n) time, and substring call takes O(n) time
+
+# avoid using class variables like self.ans
+#     class variables are for when you have an object that requires a persistent state 
 
 """
 class Solution:
@@ -46,3 +74,12 @@ class Solution:
                     ans = s[start:end+1]
         return ans
 """
+# top-down
+# time complexity: O(n^3)
+#     double nested for loop takes O(n^2) time and substring call s[start:end+1] takes O(n) time
+#     can save time by storing answer as start and end points rather than string
+# space complexity: O(n^2)
+#     wasting a lot of space because we don't care about certain start and end pairs (for example, start = 10 and end = 5)
+#     consider using {} cache instead, and store (start, end) pairs
+
+# good practice to loop through all start and end pairs in dp function
