4a334449c85724c14e33277bf4e5554deac1f68e
meganying1 <74328643+meganying1@users.noreply.github.com>
2024-03-12 13:08:16 -0700

    Update 300: Longest Increasing Subsequence.py

--- Dynamic Programming/300: Longest Increasing Subsequence.py
+++ Dynamic Programming/300: Longest Increasing Subsequence.py
@@ -23,12 +23,26 @@ class Solution:
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         length = len(nums)
+        cache = {}
         
         def dp(i, prevHighest):
             if i == length: return 0
-            if nums[i] <= prevHighest: return dp(i+1, prevHighest)
-            return max(dp(i+1, nums[i])+1, dp(i+1, prevHighest))
+            if (i, prevHighest) in cache != None: cache[(i, prevHighest)]
+            if nums[i] <= prevHighest: 
+                ans = dp(i+1, prevHighest)
+                cache[(i, prevHighest)] = ans
+                return ans
+            ans = max(dp(i+1, nums[i])+1, dp(i+1, prevHighest))
+            cache[(i, prevHighest)] = ans
+            return ans
         
         return dp(0, -float("inf"))
 """
-# top-down
+# top-down with memoization (just for practice)
+# time complexity: O(n^2)
+#     input parameters to dp function represent state space, so our state space is a function of i and prevHighest
+#     we have n*n states
+# space complexity: O(n^2)
+
+# can't define cache using just index i because there are different scenarios for the same index
+#     in practice, cache needs to line up with parameters to dp function
