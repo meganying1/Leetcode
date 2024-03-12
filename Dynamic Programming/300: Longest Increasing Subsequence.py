c2425e6033c1195651e3ad02a2b5ac02b3ec2906
meganying1 <74328643+meganying1@users.noreply.github.com>
2024-03-12 11:30:09 -0700

    Update 300: Longest Increasing Subsequence.py

--- Dynamic Programming/300: Longest Increasing Subsequence.py
+++ Dynamic Programming/300: Longest Increasing Subsequence.py
@@ -15,5 +15,20 @@ class Solution:
                 if nums[i] > nums[j]: greatest = max(greatest, dp[j])
             dp[i] += greatest
         return max(dp)
+# bottom-up
 # time complexity: O(n^2)
 # space complexity: O(n)
+
+"""
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        length = len(nums)
+        
+        def dp(i, prevHighest):
+            if i == length: return 0
+            if nums[i] <= prevHighest: return dp(i+1, prevHighest)
+            return max(dp(i+1, nums[i])+1, dp(i+1, prevHighest))
+        
+        return dp(0, -float("inf"))
+"""
+# top-down
