9c4c22e93cc95e7f5a47eb8d49b88d35c0f5fd4a
meganying1 <74328643+meganying1@users.noreply.github.com>
2024-03-17 17:50:23 -0700

    Create 139: Word Break.py

---
+++ Dynamic Programming/139: Word Break.py
@@ -0,0 +1,48 @@
+# https://leetcode.com/problems/word-break/description/
+# difficulty: medium
+# topics: array, hash table, string, dynamic programming, trie, memoization
+
+# program:
+# Given a string s and a dictionary of strings wordDict, return true if s can be segmented into a space-separated sequence of one or more dictionary words.
+# Note that the same word in the dictionary may be reused multiple times in the segmentation.
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        mod = (10**9) + 7
+        n = len(s)
+        cache = [None] * (n+1)
+
+        def getVal(c):
+            return ord(c)-ord('a')+1
+        
+        def getHash(s):
+            ans = 0
+            for c in s: ans = ((ans * 26) + getVal(c)) % mod
+            return ans
+            
+        hashDict = defaultdict(set)
+        for word in wordDict: hashDict[getHash(word)].add(word)
+
+        def dp(start):
+            if start == n: return True
+            if cache[start] != None: return cache[start]
+            hashVal = 0
+            end = start
+            while end < n:
+                hashVal = ((hashVal * 26) + getVal(s[end])) % mod
+                if hashVal in hashDict and s[start:end+1] in hashDict[hashVal]:
+                    ans = dp(end+1)
+                    cache[start] = ans
+                    if ans == True: return True
+                end += 1
+            cache[start] = False
+            return False
+
+        return dp(0)
+
+# top-down with memoization
+# time complexity: O(n^3)
+#   we have n states
+#   we loop over each state n times
+#   within each loop, we do an n substring slice
+# space complexity: O(len(wordDict) + len(s))
