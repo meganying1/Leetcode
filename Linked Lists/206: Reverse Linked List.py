711b7e2f3ba92cd2adbc33ee0b8936a152aa2647
meganying1 <74328643+meganying1@users.noreply.github.com>
2024-03-20 16:17:56 -0700

    Create 206: Reverse Linked List.py

---
+++ Linked Lists/206: Reverse Linked List.py
@@ -0,0 +1,32 @@
+# https://leetcode.com/problems/reverse-linked-list/
+# difficulty: easy
+# topics: linked list, recursion
+
+# Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        curr, prev = head, None
+        while curr:
+            nextNode = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nextNode
+        return prev 
+# iterative solution
+# time complexity: O(n)
+# space complexity: O(1)
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+        def reverse(prev, curr):
+            if curr == None: return prev
+            nextNode = curr.next
+            curr.next = prev
+            return reverse(curr, nextNode)
+
+        return reverse(None, head)
+# recursive solution
+# time complexity: O(n)
+# space complexity: O(1)
