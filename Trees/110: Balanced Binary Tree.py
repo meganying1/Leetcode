71a764b3c7446d158f54613451f0263aa88d7a55
meganying1 <74328643+meganying1@users.noreply.github.com>
2024-03-27 16:52:16 -0700

    Update 110: Balanced Binary Tree.py

--- Trees/110: Balanced Binary Tree.py
+++ Trees/110: Balanced Binary Tree.py
@@ -1,13 +1,22 @@
+# https://leetcode.com/problems/balanced-binary-tree/
+# difficulty: easy
+# topics: tree, depth-first search, binary tree
+
+# problem:
+# Given a binary tree, determine if it is height-balanced.
+
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
         ans = True
         
         def dfs(root):
             nonlocal ans
-            if not root: return 0
+            if not root or not ans: return 0
             left, right = dfs(root.left), dfs(root.right)
             if abs(left-right) > 1: ans = False
             return 1 + max(left, right)
         
         dfs(root)
         return ans
+# time complexity: O(n)
+# space complexity: O(height)
