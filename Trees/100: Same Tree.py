7292346d57e724e59bb39a80cc20d9c379cfb0de
meganying1 <74328643+meganying1@users.noreply.github.com>
2024-03-29 21:24:06 -0700

    Update 100: Same Tree.py

--- Trees/100: Same Tree.py
+++ Trees/100: Same Tree.py
@@ -11,3 +11,5 @@ class Solution:
         if not p and not q: return True
         if not p or not q: return False
         return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+# time complexity: O(min(n, m))
+# height complexity: O(min(height of p, height of q))
