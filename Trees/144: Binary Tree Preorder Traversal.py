5f99511abd735c79eb906c183027903331e24f01
meganying1 <74328643+meganying1@users.noreply.github.com>
2024-04-04 18:24:37 -0700

    Update 144: Binary Tree Preorder Traversal.py

--- Trees/144: Binary Tree Preorder Traversal.py
+++ Trees/144: Binary Tree Preorder Traversal.py
@@ -7,25 +7,14 @@
 
 class Solution:
     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if not root: return []
+        return [root.val] + self.preorderTraversal(root.left) + self.preorderTraversal(root.right)
+
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         ans = []
         if root:
             ans.append(root.val)
             if root.left: ans.extend(self.preorderTraversal(root.left))
             if root.right: ans.extend(self.preorderTraversal(root.right))
         return ans
-
-"""
-class Solution:
-    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        ans = []
-
-        def traverse(root):
-            if not root: return
-            nonlocal ans
-            ans.append(root.val)
-            traverse(root.left)
-            traverse(root.right)
-        
-        traverse(root)
-        return ans
-"""
