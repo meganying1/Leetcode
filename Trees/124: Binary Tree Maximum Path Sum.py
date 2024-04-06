c5924ecb1d205dfdd47655e18757b92782cf64b3
meganying1 <74328643+meganying1@users.noreply.github.com>
2024-04-05 21:52:43 -0700

    Update 124: Binary Tree Maximum Path Sum.py

--- Trees/124: Binary Tree Maximum Path Sum.py
+++ Trees/124: Binary Tree Maximum Path Sum.py
@@ -11,6 +11,28 @@ class Solution:
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
         ans = -float("inf")
         
+        def maxPathSumHelper(node):
+            nonlocal ans
+            if not root: return 0
+            leftMax = max(0, maxPathSumHelper(node.left))
+            rightMax = max(0, maxPathSumHelper(node.right))
+            currMax = node.val + max(leftMax, rightMax)
+            ans = max(ans, currMax, node.val + leftMax + rightMax)
+            return currMax
+        
+        maxPathSumHelper(root)
+        return ans
+# time complexity: O(n)
+# space complexity: O(height of root)
+# important takeaway:
+#     when you need to figure out some result for something, you can try to figure out the result for every X
+#     and if you try it for all X, you have the result
+
+"""
+class Solution:
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        ans = -float("inf")
+        
         def maxPathSumHelper(root):
             nonlocal ans
             if not root: return 0
@@ -22,8 +44,16 @@ class Solution:
         
         maxPathSumHelper(root)
         return ans
-# time complexity: O(n)
-# space complexity: O(height of root)
-# important takeaway:
-#     when you need to figure out some result for something, you can try to figure out the result for every X
-#     and if you try it for all X, you have the result
+"""
+# doing two things: constructing currMax and updating the answer
+# could getCurrMax function: dedicated to getting the currMax for a given node, where currMax is “the max sum starting from this node, descending down a chain children"
+#     answer to overall problem then can be separated into two steps:
+#         inner function is just a dfs function that goes through all nodes and applies getCurrMax function
+#     if we can get getCurrMax to run in O(1) time on average, overall function runs in O(n) time
+#         if we spend n time preprocessing / filling out the cache table, then all future calls will take O(1)
+#         as long as we make at least n future calls, we can say each call to the function, overall, takes constant time on average
+
+# doesn't matter the order of we check the nodes in: could be preorder, inorder, or postorder
+
+# don't shadow variables (named parameter in helper root, which shares a name with the outer function)
+#     can be unclear which root is being referred to
