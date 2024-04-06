a176489878bbae645dc70b11a4520d6f62d3676d
meganying1 <74328643+meganying1@users.noreply.github.com>
2024-04-05 22:07:03 -0700

    Update 102: Binary Tree Level Order Traversal.py

--- Trees/102: Binary Tree Level Order Traversal.py
+++ Trees/102: Binary Tree Level Order Traversal.py
@@ -7,6 +7,22 @@
 
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        queue = deque()
+        queue.append(root)
+        ans = []
+        while queue:
+            level = []
+            for i in range(len(queue)):
+                curr = queue.popleft()
+                level.append(curr.val)
+                if curr.left: queue.append(curr.left)
+                if curr.right: queue.append(curr.right)
+            ans.append(level)
+        return ans
+
+"""
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         if not root: return []
         queue = deque()
         queue.append(root)
@@ -20,19 +36,31 @@ class Solution:
                 if curr.right: queue.append(curr.right)
             ans.append(level)
         return ans
+"""
+# can use 'while queue' or 'while len(queue)' instead of 'while len(queue) > 0'
+# don't need initial case: 'if not root: return []'
 
 """
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        nodeDict = defaultdict(list)
+        levelToVals = defaultdict(list)
 
         def search(root, level):
-            nonlocal nodeDict
+            nonlocal levelToVals
             if not root: return
-            nodeDict[level].append(root.val)
+            levelToVals[level].append(root.val)
             search(root.left, level+1)
             search(root.right, level+1)
 
         search(root, 0)
-        return [val for val in nodeDict.values()]
+        return [val for val in levelToVals.values()]
 """
+# time complexity: O(n)
+# space complexity: O(height of tree)
+
+# don't need nonlocal here
+#     nonlocal exists, because in python, reassigning a variable is indistinguishable from initializing a variable, due to the syntax of the language
+#     nonlocal tells the code if we are initializing a new variable inside the function, or if we are reassigning an existing variable outside our scope
+# equals sign means we are trying to reassign or initialize, and we need to distinguish
+#     writing nonlocal nodeDict here is bad because we aren't using “=“ or trying to reassign nodeDict, we're just changing the keys/values inside it
+#     you should usually not reassign hashmaps; the point of a hashmap is to mutate it, add keys/values, etc
