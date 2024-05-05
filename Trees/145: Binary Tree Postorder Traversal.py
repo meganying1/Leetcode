0f893d8e4e9ec73b8d259310fc9bd80378d5b0e8
meganying1 <74328643+meganying1@users.noreply.github.com>
2024-05-05 10:17:04 -0700

    Update 145: Binary Tree Postorder Traversal.py

--- Trees/145: Binary Tree Postorder Traversal.py
+++ Trees/145: Binary Tree Postorder Traversal.py
@@ -1,10 +1,25 @@
-# https://leetcode.com/problems/binary-tree-inorder-traversal/description/
+# https://leetcode.com/problems/binary-tree-postorder-traversal/description/
 # difficulty: easy
 # topics: stack, tree, depth-first search, binary tree
 
 # problem: 
 # Given the root of a binary tree, return the postorder traversal of its nodes' values.
 
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        def postorder(node):
+            if not node:
+                return
+            postorder(node.left)
+            postorder(node.right)
+            res.append(node.val)
+        postorder(root)
+        return res
+# time complexity: O(n)
+# space complexity: O(height of tree)
+
+"""
 class Solution:
     def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         if not root: return []
@@ -18,3 +33,5 @@ class Solution:
             if root.right: ans.extend(self.postorderTraversal(root.right))
             ans.append(root.val)
         return ans
+"""
+# refer to binary tree preorder problem
