0a717b2d9d473bb7e6d4b9a6cac1a5531e848c59
meganying1 <74328643+meganying1@users.noreply.github.com>
2024-05-05 10:15:06 -0700

    Update 144: Binary Tree Preorder Traversal.py

--- Trees/144: Binary Tree Preorder Traversal.py
+++ Trees/144: Binary Tree Preorder Traversal.py
@@ -7,9 +7,29 @@
 
 class Solution:
     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        def preorder(node):
+            if not node:
+                return
+            res.append(node.val)
+            preorder(node.left)
+            preorder(node.right)
+        preorder(root)
+        return res
+# time complexity: O(n)
+# space complexity: O(height of tree)
+
+"""
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         if not root: return []
         return [root.val] + self.preorderTraversal(root.left) + self.preorderTraversal(root.right)
+"""
+# also be cautious of adding arrays: addition is extension
+# same time complexity as previous solution
+# usually try to have one array and pass it around rather than continuously making new arrays
 
+"""
 class Solution:
     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         ans = []
@@ -18,3 +38,8 @@ class Solution:
             if root.left: ans.extend(self.preorderTraversal(root.left))
             if root.right: ans.extend(self.preorderTraversal(root.right))
         return ans
+"""
+# be cautious of using extend
+# worst case time complexity: O(n^2)
+# best case time complexity: O(nlogn)
+    # there are logn levels to the tree and each level takes O(n) time
