ca382e6ae22d7399915dab7ce3228840f6bccc4f
meganying1 <74328643+meganying1@users.noreply.github.com>
2024-05-23 21:16:30 -0700

    Update 51: N-Queens.py

--- Backtracking/51: N-Queens.py
+++ Backtracking/51: N-Queens.py
@@ -9,6 +9,33 @@
 
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
+        queenCols, ans = [], []
+        cols, diags1, diags2 = set(), set(), set()
+
+        def backtrack(row):
+            if row == n:
+                ans.append(["".join(["." * col, "Q", "." * (n-col-1)]) for col in queenCols])
+                return
+            for col in range(n):
+                if col in cols or n-row+col in diags1 or n+row+col in diags2: continue
+                cols.add(col)
+                diags1.add(n-row+col)
+                diags2.add(n+row+col)
+                queenCols.append(col)
+                backtrack(row+1)
+                cols.remove(col)
+                diags1.remove(n-row+col)
+                diags2.remove(n+row+col)
+                queenCols.pop()
+
+        backtrack(0)
+        return ans
+# time complexity: O(n^2 * n!)
+# space complexity: O(n)
+
+"""
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
         board, ans = [], []
         cols, diags1, diags2 = set(), set(), set()
 
@@ -30,6 +57,7 @@ class Solution:
 
         backtrack(0)
         return ans
+"""
 # time complexity: O(n^2 * n!)
 #     the tree has n! nodes
 #         first layer has n nodes, second layer has n-1 nodes, etc.
