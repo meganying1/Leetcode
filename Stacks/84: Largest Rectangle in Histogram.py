17bd1fdcd2c7995eca9b3c3addb5d6883625e8e6
meganying1 <74328643+meganying1@users.noreply.github.com>
2024-05-31 22:49:06 -0700

    Update 84: Largest Rectangle in Histogram.py

--- Stacks/84: Largest Rectangle in Histogram.py
+++ Stacks/84: Largest Rectangle in Histogram.py
@@ -7,6 +7,26 @@
 
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
+        heights.append(0)
+        n = len(heights)
+        stack = []
+        ans = 0
+        for i in range(n):
+            h = heights[i]
+            start = i
+            while stack and stack[-1][1] > h:
+                prevI, prevH = stack.pop()
+                ans = max(ans, prevH * (i-prevI))
+                start = prevI
+            stack.append((start, h))
+        return ans
+# 
+# time complexity: O(n)
+# space complexity: O(n)
+
+"""
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
         n = len(heights)
         stack = []
         ans = 0
@@ -21,8 +41,13 @@ class Solution:
         for (i, h) in stack:
             ans = max(ans, h * (n-i))
         return ans
-# time complexity: O(n)
-# space complexity: O(n)
+"""
+# use a stack to store the indices and their corresponding heights
+# if the previous height at the top of the stack is greater than the current one, we can calculate the area from the previous index to the current index
+#      we're assuming that the current index is the right edge
+# since we know that that previous height was greater, we  append the current height and say it started at that previous index
+# at the end, we know that anything remaining in the stack didn’t encounter a smaller height, so we can calculate the area from that index to the end of the array
+#     can append a 0 to the end of the heights array to clean up the remaining stack
 
 """
 class Solution:
