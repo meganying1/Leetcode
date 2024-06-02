be2ba71c3d8858ad808ae4838adfe2cb4bde9c62
meganying1 <74328643+meganying1@users.noreply.github.com>
2024-06-02 14:28:12 -0700

    Update 973: K Closest Points to Origin.py

--- Heaps/973: K Closest Points to Origin.py
+++ Heaps/973: K Closest Points to Origin.py
@@ -1,3 +1,12 @@
+# https://leetcode.com/problems/k-closest-points-to-origin/
+# difficulty: medium
+# topics: array, math, divide and conquer, geometry, sorting, heap (priority queue), quickselect
+
+# problem:
+# Given an array of points where points[i] = [xi, yi] represents a point on the X-Y plane and an integer k, return the k closest points to the origin (0, 0).
+# The distance between two points on the X-Y plane is the Euclidean distance (i.e., √(x1 - x2)2 + (y1 - y2)2).
+# You may return the answer in any order. The answer is guaranteed to be unique (except for the order that it is in).
+
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
         
