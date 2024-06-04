524fc4c0e5e81b98fa1f2802cb03cf9a29444990
meganying1 <74328643+meganying1@users.noreply.github.com>
2024-06-03 23:05:37 -0700

    Create 295: Find Median from Data Stream.py

---
+++ Heaps/295: Find Median from Data Stream.py
@@ -0,0 +1,30 @@
+# https://leetcode.com/problems/find-median-from-data-stream/
+# difficulty: hard
+# topics: two pointers, design, sorting, heap (priority queue), data stream)
+
+# problem:
+# The median is the middle value in an ordered integer list. If the size of the list is even, there is no middle value, and the median is the mean of the two middle values.
+#   For example, for arr = [2,3,4], the median is 3.
+#   For example, for arr = [2,3], the median is (2 + 3) / 2 = 2.5.
+# Implement the MedianFinder class:
+#   MedianFinder() initializes the MedianFinder object.
+#   void addNum(int num) adds the integer num from the data stream to the data structure.
+#   double findMedian() returns the median of all elements so far. Answers within 10-5 of the actual answer will be accepted.
+
+class MedianFinder:
+
+    def __init__(self):
+        self.minHeap = []
+        self.maxHeap = []
+        heapq.heapify(self.minHeap)
+        heapq.heapify(self.maxHeap)
+
+    def addNum(self, num: int) -> None:
+        if len(self.minHeap) == len(self.maxHeap):
+            heapq.heappush(self.minHeap, -heapq.heappushpop(self.maxHeap, -num))
+        else:
+            heapq.heappush(self.maxHeap, -heapq.heappushpop(self.minHeap, num))
+
+    def findMedian(self) -> float:
+        if len(self.minHeap) == len(self.maxHeap): return (self.minHeap[0]-self.maxHeap[0])/2
+        return self.minHeap[0]
