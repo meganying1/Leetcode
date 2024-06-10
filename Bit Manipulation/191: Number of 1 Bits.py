4da95bdf9159392035ecf46bb5bcc5e83f3da6a2
meganying1 <74328643+meganying1@users.noreply.github.com>
2024-06-09 21:27:18 -0700

    Update 191: Number of 1 Bits.py

--- Bit Manipulation/191: Number of 1 Bits.py
+++ Bit Manipulation/191: Number of 1 Bits.py
@@ -15,3 +15,9 @@ class Solution:
 # time complexity: O(k)
 #   k is the number of bits in n
 # space complexity: O(1)
+
+# need to think about how while loop works: for the computer to check if a number is not 0, it has to check if any of the bits are set
+#     assuming n is a 32-bit integer, the machine has to do a scan of all 32 bits every time it needs to check if n is > 0 
+#     so the while loop takes O(32) to run one iteration in a way, but we already know that our number has say 32 bits at most
+# instead we can do: for offset in range(32): (check if the offset-th bit is set, and if so add 1 to the result)
+#     in practice doesn't matter since bit operations are very very quick
