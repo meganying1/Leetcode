4e376abd565c34608d237bcc0c15ea0bde493936
meganying1 <74328643+meganying1@users.noreply.github.com>
2024-07-02 21:26:07 -0700

    Update 994: Rotting Oranges.py

--- Graphs/994: Rotting Oranges.py
+++ Graphs/994: Rotting Oranges.py
@@ -14,25 +14,24 @@ class Solution(object):
     def orangesRotting(self, grid):
         rows, cols = len(grid), len(grid[0])
         visited = set()
-        ans = fresh = 0
+        t = fresh = 0
         queue = deque()
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c] == 2: queue.append((r, c, 0))
+                if grid[r][c] == 2: queue.append((r, c, t))
                 elif grid[r][c] == 1: fresh += 1
 
         def isValid(r, c):
             return r >= 0 and r < rows and c >= 0 and c < cols
-        
+
         while queue:
             (r, c, t) = queue.popleft()
-            ans = max(ans, t)
             for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
                 newR, newC = r+dx, c+dy
                 if not isValid(newR, newC) or grid[newR][newC] != 1 or (newR, newC) in visited: continue
                 fresh -= 1
                 visited.add((newR, newC))
                 queue.append((newR, newC, t+1))
-        return ans if fresh == 0 else -1
+        return t if fresh == 0 else -1
 # time complexity: O(n*m)
 # space complexity: O(n*m)
