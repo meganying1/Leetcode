f8ec577a7ca2e88705bb95400e021097d40d294f
meganying1 <74328643+meganying1@users.noreply.github.com>
2024-07-02 18:14:56 -0700

    Update 200: Number of Islands.py

--- Graphs/200: Number of Islands.py
+++ Graphs/200: Number of Islands.py
@@ -28,3 +28,4 @@ class Solution(object):
 # time complexity: O(n*m)
 # space complexity: O(n*m)
 # using a set for seen values is more efficient here because we may not need to store information about all cells in the matrix
+# using an array would be more efficient if all of the cells are equal to "1" since arrays are more performant than hashmaps
