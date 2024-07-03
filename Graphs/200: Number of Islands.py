9920212a99deb1583e44160974832dccdfda839e
meganying1 <74328643+meganying1@users.noreply.github.com>
2024-07-02 18:21:58 -0700

    Update 200: Number of Islands.py

--- Graphs/200: Number of Islands.py
+++ Graphs/200: Number of Islands.py
@@ -25,8 +25,14 @@ class Solution(object):
                     ans += 1
                     dfs(i, j)
         return ans
+# this solution uses depth-first search
 # time complexity: O(n*m)
 # space complexity: O(n*m)
-#     if we ignore the set, we still use n*m space since the depth of the callstack is n*m
+#     if we ignore the set, we still could use up to n*m space if the grid is snake-like:
+#         1 0 1 1 1
+#         1 0 1 0 1
+#         1 0 1 0 1
+#         1 1 1 0 1
+#     for a full grid of land, the stack space is just the depth, and the grid traversal won't move in a way that uses n*m space
 # using a set for seen values is more efficient here because we may not need to store information about all cells in the matrix
 # using an array would be more efficient if all of the cells are equal to "1" since arrays are more performant than hashmaps
