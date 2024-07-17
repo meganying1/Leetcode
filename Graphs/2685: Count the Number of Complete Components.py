6232c16c26109eddab35386f154845ea02f056ca
meganying1 <74328643+meganying1@users.noreply.github.com>
2024-07-16 18:19:17 -0700

    Update 2685: Count the Number of Complete Components.py

--- Graphs/2685: Count the Number of Complete Components.py
+++ Graphs/2685: Count the Number of Complete Components.py
@@ -16,19 +16,52 @@ class Solution:
             neighborMap[node1].append(node2)
             neighborMap[node2].append(node1)
         
-        def dfs(node, num, e, v):
+        def dfs(node, compNodes):
+            seen[node] = True
+            compNodes.append(node)
+            for neighbor in neighborMap[node]:
+                if not seen[neighbor]: v = dfs(neighbor, compNodes)
+            return compNodes
+        
+        ans = 0
+        for i in range(n):
+            if seen[i]: continue
+            compNodes = dfs(i, [])
+            compSize = len(compNodes)
+            if all(len(neighborMap[node]) == compSize-1 for node in compNodes): ans += 1
+        return ans
+# time complexity: O(v+e)
+# space complexity: O(v+e)
+#     space complexity will probably never be O(e) for any problem
+#     neighborMap is always O(v) space for the keys, since each node is a key, and O(e) space for the values, as we store every edge in a neighbor array
+#     callstack depth on dfs is usually O(v) too
+
+"""
+class Solution:
+    def countCompleteComponents(self, n: int, edges: List[List[int]]) -> int:
+        neighborMap = defaultdict(list)
+        seen = [False for _ in range(n)]
+        for node1, node2 in edges:
+            neighborMap[node1].append(node2)
+            neighborMap[node2].append(node1)
+        
+        def dfs(node, e, v):
             seen[node] = True
             v += 1
             for neighbor in neighborMap[node]:
                 e += 1
-                if not seen[neighbor]: e, v = dfs(neighbor, num, e, v)
+                if not seen[neighbor]: e, v = dfs(neighbor, e, v)
             return e, v
         
-        ans = num = 0
+        ans = 0
         for i in range(n):
             if seen[i]: continue
-            e, v = dfs(i, num, 0, 0)
+            e, v = dfs(i, 0, 0)
             e //= 2
-            num += 1
             if v*(v-1)//2 == e: ans += 1
         return ans
+"""
+# common pattern is to pass in a set or array as a parameter of dfs to accumulate or keep track of things
+# we don't need to deal with edges in the dfs function
+# we know that if a component has k nodes, each node in that component needs to have exactly k-1 edges for that component to be perfect
+# we know how many edges each node has from neighborMap
