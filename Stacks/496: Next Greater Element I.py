41921be6f02ba5ba58a39055a96d71f4b7d889d9
meganying1 <74328643+meganying1@users.noreply.github.com>
2024-07-29 21:44:17 -0700

    Update 496: Next Greater Element I.py

--- Stacks/496: Next Greater Element I.py
+++ Stacks/496: Next Greater Element I.py
@@ -10,7 +10,7 @@
 
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        stack, ans = [], []
+        stack = []
         greaterElemDict = {}
         for i in range(len(nums2)):
             while stack and stack[-1] < nums2[i]:
