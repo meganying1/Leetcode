eb20e4ac278df763661a8f6123092906853c9c01
meganying1 <74328643+meganying1@users.noreply.github.com>
2024-08-21 10:19:49 -0700

    Update 40: Combination Sum II.py

--- Backtracking/40: Combination Sum II.py
+++ Backtracking/40: Combination Sum II.py
@@ -7,6 +7,34 @@
 # Each number in candidates may only be used once in the combination.
 # Note: The solution set must not contain duplicate combinations.
 
+class Solution(object):
+    def combinationSum2(self, candidates, target):
+        candidates.sort()
+        n = len(candidates)
+        path = []
+        ans = []
+        
+        def backtrack(i, totalSum):
+            if totalSum == target:
+                ans.append(path[:])
+                return
+            if totalSum > target or i == n: return
+            for j in range(i, n):
+                if j > i and candidates[j] == candidates[j-1]: continue
+                path.append(candidates[j])
+                backtrack(j+1, totalSum+candidates[j])
+                path.pop()
+        
+        backtrack(0, 0)
+        return ans
+# time complexity: O(n * 2^n)
+    # size of tree: 2^n
+        # branching factor is 2 and depth of tree is n
+    # time complexity of each node: O(n)
+# space complexity: O(sort + n)
+    # minimum amount of space needed is O(max(sort, n))
+    # callstack depth is n and max length of path is n
+
 """
 class Solution(object):
     def combinationSum2(self, candidates, target):
@@ -32,10 +60,5 @@ class Solution(object):
         backtrack(0, 0)
         return ans
 """
-# time complexity: O(n * 2^n)
-    # size of tree: 2^n
-        # branching factor is 2 and depth of tree is n
-    # time complexity of each node: O(n)
-# space complexity: O(sort + n)
-    # minimum amount of space needed is O(max(sort, n))
-        # we can reusue space from sort for target, but only if it is garbage collected quickly enough
+# we can iterate over entire candidates array starting from current index i
+    # if current candidate is same as previous one, we skip it to avoid duplicates
