1be7a4ab449c1aed8b84e4190777cfed8b1d5c51
meganying1 <74328643+meganying1@users.noreply.github.com>
2024-08-25 21:17:33 -0700

    Update 91: Decode Ways.py

--- Dynamic Programming/91: Decode Ways.py
+++ Dynamic Programming/91: Decode Ways.py
@@ -27,8 +27,7 @@ class Solution:
         dp[1] = 1
         for i in range(2, n+1):
             if s[i-1] != "0": dp[i] += dp[i-1]
-            twoDig = int(s[i-2:i])
-            if 10 <= twoDig and twoDig <= 26: dp[i] += dp[i-2]
+            if s[i-2] == "1" or (s[i-2] == "2" and s[i-1] in "123456"): dp[i] += dp[i-2]
         return dp[-1]
 # time complexity: O(n)
 # space complexity: O(n)
